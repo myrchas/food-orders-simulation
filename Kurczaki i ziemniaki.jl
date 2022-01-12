f1eea93ed3b691e7d81d19b30404289abd31be73
Marcin Koc <marcinkoc@Marcins-MacBook-Air.local>
2022-01-12 20:25:13 +0100

    plik tekstowy i pierwsze komentarze

---
+++ Kurczaki i ziemniaki.jl
@@ -0,0 +1,25 @@
+##Pakiety
+
+
+##Zmienne
+#Menu z cenami
+
+#Produkty i ich data waznosci
+
+#Początkowy stan produktów
+
+##Rozkłady 
+#Ilość samochodów na minutę
+
+##Główna funkcja kroku
+#jeden krok = jedna minuta
+
+function OneStep()
+
+    #wylosować z rozkładu samochdów na minutę ile samochdów przyjedzie w tym kroku
+
+    #wylosować ilość osób w kazdym samochodzie, 1 samochod = jedna zmienna?
+
+    # wylosować zamówienia dla kazdego samochodu
+
+    #
